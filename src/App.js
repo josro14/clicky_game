cc46c14233197a3623854acb67de5b4a5d3e8b3e
teej107 <teej107@gmail.com>
2018-06-25 22:46:04 -0600

    App stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Nav from "./components/Nav/Nav";
+import Header from './components/Header/Header';
 import "./App.css";
 
+//===================================================================================
 //REDUX TUTORIAL
 //https://www.youtube.com/watch?v=1w-oQ-i1XB8&list=PLoYCgNOIyGADILc3iUJzygCqC8Tt3bRXt
+//===================================================================================
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-          <Nav/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Nav/>
+                <Header/>
+            </div>
+        );
+    }
 }
 
 
