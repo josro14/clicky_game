8b3fb392f93f9016bc8339b1c2ce7f46a6ed9a2a
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-06-25 21:11:32 -0600

    added files and app.js code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ClickItem from "./components/ClickItem";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Game from "./components/Game";
+import Header from "./components/Nav";
+import NavMessage from "./components/NavMessage";
+
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+  state = {
+
+  };
+
+
+};
+
 
 export default App;
